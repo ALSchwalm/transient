e07cf66883f6188c52ebe317f62f520cbc4632ac
Adam Schwalm <adamschwalm@gmail.com>
2021-07-23 20:39:05 -0500

    test: Add unittests for utils.lock_file

--- test/unit/test_utils.py
+++ test/unit/test_utils.py
@@ -1,4 +1,7 @@
 import pytest
+import tempfile
+import time
+
 import transient.utils as u
 
 # fmt: off
@@ -41,7 +44,6 @@ import transient.utils as u
     # function to make 'entries' part of the test name more legible
     ids=lambda arg: '/'.join(map(repr, arg)) if isinstance(arg, tuple) else None,
 )
-# fmt: on
 def test_prompt_yes_no(default, entries, expected, monkeypatch):
     entry = iter(entries)
 
@@ -50,3 +52,39 @@ def test_prompt_yes_no(default, entries, expected, monkeypatch):
 
     monkeypatch.setattr('builtins.input', mock_input)
     assert u.prompt_yes_no("test prompt", default) == expected
+
+# fmt: on
+def test_lock_file():
+    with tempfile.NamedTemporaryFile() as f:
+        with u.lock_file(f.name, "r", timeout=0) as locked:
+            pass
+
+
+def test_lock_file_unlocks():
+    with tempfile.NamedTemporaryFile() as f:
+        with u.lock_file(f.name, "r", timeout=0) as locked:
+            pass
+
+        # We should be able to get the lock again
+        with u.lock_file(f.name, "r", timeout=0) as locked:
+            pass
+
+
+@pytest.mark.xfail(raises=OSError)
+def test_lock_file_not_recursive():
+    with tempfile.NamedTemporaryFile() as f:
+        with u.lock_file(f.name, "r", timeout=0) as locked:
+            with u.lock_file(f.name, "r", timeout=0) as second_lock:
+                pass
+
+
+def test_lock_file_timeout():
+    with tempfile.NamedTemporaryFile() as f:
+        with u.lock_file(f.name, "r", timeout=0) as locked:
+            lock_timeout = 0.5
+            start_time = time.time()
+            try:
+                with u.lock_file(f.name, "r", timeout=lock_timeout) as second_lock:
+                    pass
+            except OSError:
+                assert time.time() - start_time >= lock_timeout
